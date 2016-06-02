39ace2c84354b97aa3b8bb6490f14be96a8e18c5
Cesar Eduardo Gonzalez Borjon <cesar.e.gonzalez@oracle.com>
2016-06-01 21:19:18 -0500

    fixing constants

--- client/app/constants/constants.service.js
+++ client/app/constants/constants.service.js
@@ -4,8 +4,8 @@ angular.module('processAdminApp.constants')
   .factory('constants', function () {
     var factory = {};
 
-    factory.LOCAL_API_ENDPOINT = "http://localhost:8080";
-    factory.PROD_API_ENDPOINT = "http://tersuslavanderia.com:8080";
+    factory.LOCAL_API_ENDPOINT = "http://localhost:8080/api";
+    factory.PROD_API_ENDPOINT = "http://tersuslavanderia.com:8080/api";
     factory.API_ENDPOINT = "";
 
     return factory;
